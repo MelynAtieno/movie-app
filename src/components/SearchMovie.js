8901bb621fabf5fb7a17cc100e698e22324db40f
Melyn Atieno Were <melynatieno@gmail.com>
2022-08-01 09:28:28 +0300

    Updated code for the searchbox

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,27 +1,56 @@
 import React from 'react';
 
-
-function SearchMovie (props) {
-    function onSearch(e) {
-        e.preventDefault();
-        fetch("https://api.themoviedb.org/3/movie/550?api_key=b082632c7b887de3dbcf4651f0252347", {
-          method: "POST",
-        });
-        console.log();
-        
-      }
-    
+function SearchMovie ({ handleInput, search }) {
 	return (
-		<div className="search">
-			<input
-				className="input"
-				value={props.value}
-				onChange={(event) => props.setSearchValue(event.target.value)}
-				placeholder="Enter name of movie..."
-			></input>
-            <button class="review" onClick={onSearch}><b>REVIEW</b></button>
-		</div>
-	);
-};
+		<section className="searchbox">
+			<input 
+				type="text" 
+				placeholder="Enter name of movie..." 
+				className="search" 
+				onChange={handleInput}
+				onKeyPress={search}
+			/>
+		</section>
+	)
+}
+
+
+
+// function SearchMovie () {
+//     const [searchValue, setSearchValue] = useState("");
+
+//     function resetInputField() {
+//         setSearchValue("");
+//       };
+    
+
+//     function search (e, search) {
+//         e.preventDefault();
+//         search(searchValue);
+//         resetInputField();
+//       };
+
+//     function onSearch(e) {
+//         e.preventDefault();
+//         fetch("https://api.themoviedb.org/3/movie/550?api_key=b082632c7b887de3dbcf4651f0252347", {
+//           method: "POST",
+//         });
+//         console.log();
+        
+//       }
+    
+// 	return (
+// 		<div className="search">
+// 			<input
+//                 type="text"
+				
+// 				value={searchValue}
+// 				onChange= {search}
+// 				placeholder="Enter name of movie..."
+// 			></input>
+//             <button class="review" onClick={onSearch}><b>SEARCH</b></button>
+// 		</div>
+// 	);
+// };
 
 export default SearchMovie;
