9b7ba3a9a70bed58b0fee4c657151d8486065f5a
Melyn Atieno Were <melynatieno@gmail.com>
2022-08-01 11:07:23 +0300

    Update search movie

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -14,43 +14,4 @@ function SearchMovie ({ handleInput, search }) {
 	)
 }
 
-
-
-// function SearchMovie () {
-//     const [searchValue, setSearchValue] = useState("");
-
-//     function resetInputField() {
-//         setSearchValue("");
-//       };
-    
-
-//     function search (e, search) {
-//         e.preventDefault();
-//         search(searchValue);
-//         resetInputField();
-//       };
-
-//     function onSearch(e) {
-//         e.preventDefault();
-//         fetch("https://api.themoviedb.org/3/movie/550?api_key=b082632c7b887de3dbcf4651f0252347", {
-//           method: "POST",
-//         });
-//         console.log();
-        
-//       }
-    
-// 	return (
-// 		<div className="search">
-// 			<input
-//                 type="text"
-				
-// 				value={searchValue}
-// 				onChange= {search}
-// 				placeholder="Enter name of movie..."
-// 			></input>
-//             <button class="review" onClick={onSearch}><b>SEARCH</b></button>
-// 		</div>
-// 	);
-// };
-
 export default SearchMovie;
